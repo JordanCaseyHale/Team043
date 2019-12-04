ca597056dd9f3446796f339daf3eea47647d5142
Joe <jdmworley1@sheffield.ac.uk>
2019-12-04 14:49:04 +0000

    Databse change

--- assignment/ReviewerTasks.java
+++ assignment/ReviewerTasks.java
@@ -7,23 +7,45 @@ public class ReviewerTasks {
 	 * Gets the possible articles for a reviewer to choose from
 	 */
 	public static void getReviewsList(String author) {
+		//only show review list if the reviewer needs to select more to review
+		//otherwise should not have access to other submissions so dont show the list
 		
-		//Get info of author
-		//Get list of submissions where there are no conflicts
-		
+		boolean show = false;
 		try(Connection con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team0043","team043","38796815")){
 			Statement stmt = con.createStatement();
-			String str = String.format("SELECT Affiliation FROM account WHERE submissionAuthor.Email = %s AND account.Email = submissionAuthor.Email", author);
-			ResultSet results = stmt.executeQuery(str);
-			String affil = results.getString(1);
-			str = String.format("SELECT ?? FROM submission WHERE account.Affiliation != %s AND subbmisionAuthors.Email = account.Email",affil);
-			results = stmt.executeQuery(str);
-			while(results.next()) {
-				//output list
+			String str = String.format("SELECT RemainingReviews FROM reviewers WHERE RevId = %d", "VARIABLE");
+			ResultSet result = stmt.executeQuery(str);
+			int left = result.getInt(1);
+			
+			str = String.format("SELECT COUNT FROM reviews WHERE RevID = %d", "VARIABLE");
+			result = stmt.executeQuery(str);
+			int inProgress = result.getInt(1); 
+			if(left != inProgress) {
+				show = true;
 			}
 		}
 		catch (SQLException ex){
 			ex.printStackTrace();
+		}
+		
+		
+		if(show) {
+			//Get info of author
+			//Get list of submissions where there are no conflicts
+			try(Connection con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team0043","team043","38796815")){
+				Statement stmt = con.createStatement();
+				String str = String.format("SELECT Affiliation FROM account WHERE submissionAuthor.Email = %s AND account.Email = submissionAuthor.Email", author);
+				ResultSet results = stmt.executeQuery(str);
+				String affil = results.getString(1);
+				str = String.format("SELECT ?? FROM submission WHERE account.Affiliation != %s AND subbmisionAuthors.Email = account.Email",affil);
+				results = stmt.executeQuery(str);
+				while(results.next()) {
+					//output list
+				}
+			}
+			catch (SQLException ex){
+				ex.printStackTrace();
+			}
 		}
 		
 	}
@@ -73,12 +95,19 @@ public class ReviewerTasks {
 	}
 	
 	public static void checkLast() {
+		
 		try(Connection con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team0043","team043","38796815")){
 			Statement stmt = con.createStatement();
-			String str = String.format("SELECT SubID FROM reviews WHERE RevID = %d", "VARIABLE");
+			String str = String.format("SELECT RemainingReviews FROM reviewers WHERE RevID = ", "VARIABLE");
 			ResultSet results = stmt.executeQuery(str);
-			if(results == null) {
+			int left = results.getInt(1);
+			if(left == 1) {
 				removeReviewer();
+			}
+			else {
+				left -= 1;
+				str = String.format("UPDATE reviewers SET RemainingReviews = %d WHERE RevID = %d", left "VARIABLE");
+				MySQLConnection.doUpdate(str);
 			}
 		}
 		catch (SQLException ex){
